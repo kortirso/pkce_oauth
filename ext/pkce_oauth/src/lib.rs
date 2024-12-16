68f4c7c559699cf5eefe897c32b0b98e80b4a338
Bogdanov Anton <kortirso@gmail.com>
2024-12-16 13:04:23 +0300

    add generating code verifier and challenge by rust

--- ext/pkce_oauth/src/lib.rs
+++ ext/pkce_oauth/src/lib.rs
@@ -1,12 +1,29 @@
 use magnus::{function, prelude::*, Error, Ruby};
+use rand::{thread_rng, Rng};
+use rand::distributions::Alphanumeric;
+use sha256::digest;
 
-fn hello(subject: String) -> String {
-    format!("Hello from Rust, {subject}!")
+fn generate_limited_code_verifier(length: usize) -> String {
+  thread_rng()
+    .sample_iter(&Alphanumeric)
+    .take(length)
+    .map(char::from)
+    .collect()
+}
+
+fn generate_code_verifier() -> String {
+  generate_limited_code_verifier(128)
+}
+
+fn generate_code_challenge(code_verifier: String) -> String {
+  digest(code_verifier)
 }
 
 #[magnus::init]
 fn init(ruby: &Ruby) -> Result<(), Error> {
-    let module = ruby.define_module("PkceOauth")?;
-    module.define_singleton_method("hello", function!(hello, 1))?;
-    Ok(())
+  let module = ruby.define_module("PkceOauth")?;
+  module.define_singleton_method("generate_limited_code_verifier", function!(generate_limited_code_verifier, 1))?;
+  module.define_singleton_method("generate_code_verifier", function!(generate_code_verifier, 0))?;
+  module.define_singleton_method("generate_code_challenge", function!(generate_code_challenge, 1))?;
+  Ok(())
 }
--- spec/pkce_oauth_spec.rb
+++ spec/pkce_oauth_spec.rb
@@ -5,7 +5,14 @@ RSpec.describe PkceOauth do
     expect(PkceOauth::VERSION).not_to be_nil
   end
 
-  it 'does something useful' do
-    expect(described_class.hello('User')).to eq 'Hello from Rust, User!'
+  it 'has class methods written in rust', :aggregate_failures do
+    code_verifier = described_class.generate_code_verifier
+    expect(code_verifier.size).to eq 128
+
+    code_verifier = described_class.generate_limited_code_verifier(64)
+    expect(code_verifier.size).to eq 64
+
+    code_challenge = described_class.generate_code_challenge('verifier')
+    expect(code_challenge.size).to eq 64
   end
 end
